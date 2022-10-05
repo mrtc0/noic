2e577dca5331558606e2a301a4dab534d2d6eac6
Kohei Morita <mrtc0@ssrf.in>
2022-10-05 21:37:05 +0900

    feat: impl kill
    Signed-off-by: Kohei Morita <mrtc0@ssrf.in>

---
+++ cmd/kill.go
@@ -0,0 +1,35 @@
+package cmd
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/mrtc0/noic/pkg/container"
+	"github.com/urfave/cli"
+)
+
+var KillCommand = cli.Command{
+	Name:  "kill",
+	Usage: "kill a container",
+	ArgsUsage: `<container-id>
+
+Where "<container-id>" is your name for instance of the container.
+	`,
+	Action: func(context *cli.Context) error {
+		id := context.Args().First()
+		if id == "" {
+			return errors.New("container id cannnot be empty")
+		}
+
+		c, err := container.FindByID(id)
+		if err != nil {
+			return err
+		}
+
+		if c.CurrentStatus() == container.Stopped {
+			return fmt.Errorf("container is not running")
+		}
+
+		return c.Kill()
+	},
+}
